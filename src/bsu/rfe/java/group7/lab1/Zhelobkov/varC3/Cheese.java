e5d332a466c6228adc89d50fef1a72d57d8b333e
Vitalikz <vitalikzhelobkov@gmail.com>
2021-11-21 22:15:44 +0300

    Added subsidiary classes

---
+++ src/bsu/rfe/java/group7/lab1/Zhelobkov/varC3/Cheese.java
@@ -0,0 +1,24 @@
+package bsu.rfe.java.group7.lab1.Zhelobkov.varC3;
+public class Cheese extends Food {
+    public Cheese() {
+        super("Cheese");
+        amountOfParams=0;
+    }
+
+    public void consume() {
+        System.out.println(super.getName());
+    }
+    @Override
+    public boolean equals(Object cheese) {
+        if (super.getName().equals(((String[])cheese)[0])){
+            return true;
+        }
+        return  false;
+    }
+
+    @Override
+    public int calculateCalories() {
+    calories = 25;
+    return calories;
+    }
+}
